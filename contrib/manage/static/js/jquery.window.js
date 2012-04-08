6a6e74be0594df841812d75caf32828bf7c01bd8
vuuvv <vuuvv@qq.com>
2012-04-09 01:57:00 +0800

    add window ui

--- contrib/manage/static/js/jquery.window.js
+++ contrib/manage/static/js/jquery.window.js
@@ -70,8 +70,69 @@ Window.prototype = {
 	}
 };
 
-$.fn.window = function() {
-	new Window({container: this});
-}
+//$.fn.window = function() {
+//	new Window({container: this});
+//}
+
+})(jQuery);
+(function($){
+
+$.widget("vuuvv.window", {
+	options: {
+		width: 400,
+		height: 300,
+		theme: "vista",
+		left_width: 15,
+		right_width: 15,
+		top_height: 36,
+		bottom_height: 15,
+		container: "body"
+	},
+
+	_create: function() {
+		var options = this.options,
+			w = options.width,
+			h = options.height,
+			lw = options.left_width,
+			rw = options.right_width,
+			th = options.top_height,
+			bh = options.bottom_height;
+			self = this,
+			win = (self.win = $("<div>"))
+				.addClass("vuuvv-window " + options.theme)
+				.css({"width": w + lw + rw, "height": h + th + bh})
+				.appendTo(options.container);
+
+		this._create_div("left-top", {left: 0, top: 0});
+		this._create_div("top", {left: lw, top: 0, width: w});
+		this._create_div("right-top", {left: lw + w, top: 0});
+		this._create_div("left", {left: 0, top: th, height: h});
+		this._create_div("content", {left: lw, top: th}).append(this.element);
+		this._create_div("right", {left: lw + w, top: th, height: h});
+		this._create_div("left-bottom", {left: 0, bottom: 0});
+		this._create_div("bottom", {left: lw, bottom: 0, width: w});
+		this._create_div("right-bottom", {right: 0, bottom: 0});
+
+		self._make_draggable();
+	},
+
+	_create_div: function(class_name, attr) {
+		var $dom = $("<div>");
+		$dom.addClass(class_name);
+		$dom.css(attr);
+		this.win.append($dom);
+		return $dom;
+	},
+
+	_make_draggable: function() {
+		var self = this,
+			options = self.options;
+
+		self.win.draggable({
+			handle: ".top"
+		});
+	}
+
+});
 
 })(jQuery);
