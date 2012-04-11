e90d58606f4626b959f50018cd8b9d5284b73220
Zeng Ying <vuuvv@qq.com>
2012-04-11 17:32:10 +0800

    window min effect

--- contrib/manage/static/js/jquery.window.js
+++ contrib/manage/static/js/jquery.window.js
@@ -27,7 +27,7 @@ $.widget("vuuvv.window", {
 		draggable: true,
 		width: 400,
 		height: 300,
-		left_width: 15,
+		left_width: 16,
 		right_width: 16,
 		top_height: 35,
 		bottom_height: 15,
@@ -47,16 +47,16 @@ $.widget("vuuvv.window", {
 				.addClass("vuuvv-window " + options.theme)
 				.css({"width": w + lw + rw, "height": h + th + bh})
 				.appendTo(options.container);
-
 		self._left_top = this._create_div("left-top", {left: 0, top: 0});
-		self._top = this._create_div("top", {left: lw, top: 0, width: w + 1});
+		self._top = this._create_div("top", {left: lw - 1, top: 0, width: w + 2});
 		self._right_top = this._create_div("right-top", {right: 0, top: 0});
-		self._left = this._create_div("left", {left: 0, top: th, height: h});
-		self._content = this._create_div("content", {left: lw + 1, top: th + 1}).append(this.element);
-		self._right = this._create_div("right", {right: 0, top: th, height: h});
+		self._left = this._create_div("left", {left: 0, top: th, height: h}).text(" ");
+		self._content = this._create_div("content", {left: lw, top: th + 1, width: w, height: h}).append(this.element);
+		self._right = this._create_div("right", {right: 0, top: th, height: h}).text(" ");
 		self._left_bottom = this._create_div("left-bottom", {left: 0, bottom: 0});
-		self._bottom = this._create_div("bottom", {left: lw, bottom: 0, width: w});
+		self._bottom = this._create_div("bottom", {left: lw - 1, bottom: 0, width: w + 1});
 		self._right_bottom = this._create_div("right-bottom", {right: 0, bottom: 0});
+		self._title = this._create_div("title", {left: 0, top: 0, width: w});
 
 		var close = $('<div><div class="close"></div></div>').appendTo(self.win);
 		self._hoverable(close);
@@ -88,20 +88,27 @@ $.widget("vuuvv.window", {
 
 	_make_resizable: function() {
 		var self = this;
+		self._resize_x = self._create_div("resize-x", {right: 5, top: self._right.css("top"), height: self._right.height()}).text(" ");
+		self._resize_y = self._create_div("resize-y", {left: self._bottom.css("left"), bottom: 5, width: self._bottom.width()}).text(" ");
+		self._resize = self._create_div("resize", {right: 5, bottom: 5}).text(" ");
 		this.win.resizable({
+			minWidth: 150,
+			minHeight: 51,
 			handles: {
-				s: self._bottom.addClass("ui-resizable-handle ui-resizable-s"),
-				e: self._right.addClass("ui-resizable-handle ui-resizable-e"),
-				se: self._right_bottom.addClass("ui-resizable-handle ui-resizable-se")
+				s: self._resize_y.addClass("ui-resizable-handle ui-resizable-s"),
+				e: self._resize_x.addClass("ui-resizable-handle ui-resizable-e"),
+				se: self._resize.addClass("ui-resizable-handle ui-resizable-se")
 			},
 
 			resize: function(event, ui) {
 				self._left.css("height", self.win.height() - self._left_top.height() - self._left_bottom.height());
 				self._right.css("height", self.win.height() - self._right_top.height() - self._right_bottom.height());
-				console.log(self.win.width(), self._left_top.width(), self._right_top.width());
-				console.log(self.win.width() - self._left_top.width() - self._right_top.width());
 				self._top.css("width", self.win.width() - self._left_top.width() - self._right_top.width());
 				self._bottom.css("width", self.win.width() - self._left_bottom.width() - self._right_bottom.width());
+				self._resize_x.css("height", self._right.css("height"));
+				self._resize_y.css("width", self._bottom.css("width"));
+				self._title.css("width", self._top.css("width"));
+				self._content.css({"width": self.win.width() - self.options.left_width - self.options.right_width, "height": self.win.height() - self.options.top_height - self.options.bottom_height})
 			}
 		});
 	},
@@ -111,7 +118,7 @@ $.widget("vuuvv.window", {
 			options = self.options;
 
 		self.win.draggable({
-			handle: ".top"
+			handle: ".title"
 		});
 	}
 
