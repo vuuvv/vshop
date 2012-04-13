8267717ed6e25b1902a206e2602cd5be1a418953
Zeng Ying <vuuvv@qq.com>
2012-04-13 17:28:32 +0800

    icons draggable

--- contrib/manage/static/js/vuuvv/jquery.app.js
+++ contrib/manage/static/js/vuuvv/jquery.app.js
@@ -3,18 +3,25 @@
 $.widget("vuuvv.app", {
 	options: {
 		type: "normal",
-		img: "",
+		img: "http://2.web.qstatic.com/webqqpic/pubapps/0/2/images/big.png",
 		title: ""
 	},
 
 	_create: function() {
 		var options = this.options,
 			app = this.element.addClass("app_" + options.type),
-			overlay = $("<div>").appendTo(app),
+			overlay = $("<div>").css("height", "100%").appendTo(app),
 			container = this.container = $("<div>").addClass("app_container").appendTo(overlay);
-		$("<div>").addClass("app_icon").css({
-			background: "url(" + options.img + ") no-repeat"
-		}).appendTo(container);
+		if ($.browser.msie) {
+			$("<div>").addClass("app_icon").css({
+				filter:"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + options.img + "', sizingMethod='scale')"
+			}).appendTo(container);
+		} else {
+			$("<img>").addClass("app_icon").attr({
+				src: options.img
+			}).appendTo(container);
+		}
+		$("<div>").addClass("app_name").appendTo(container).text(options.title);
 		this._hoverable(overlay);
 	}
 });
--- contrib/manage/static/js/vuuvv/jquery.desktop.js
+++ contrib/manage/static/js/vuuvv/jquery.desktop.js
@@ -1,17 +1,102 @@
 (function ($) {
 	$.widget("vuuvv.desktop", {
+		options: {
+			show_dock: true
+		},
+
 		_create: function() {
+			this.apps = [];
+			for (var i = 0; i < 30; i++) {
+				this.apps.push({order: i * 2, img: 'http://2.web.qstatic.com/webqqpic/pubapps/0/2/images/big.png', title: '金山快盘' + i, action: $.noop});
+			}
 			this.desktop = this.element;
+			this.apps_box = $("<div>").appendTo(this.desktop);
 			this._create_dock();
-			this._add_app();
+			this._create_apps();
+			this._arrange_apps();
 		},
 
 		_create_dock: function() {
 			var dock = $("<div>").dock().appendTo(this.desktop).position({of: this.desktop, at: "bottom", my: "bottom"});
 		},
 
-		_add_app: function(img) {
-			$("<div>").app().appendTo(this.desktop);
+		_create_apps: function() {
+			var self = this,
+				apps = this.apps,
+				_apps = {},
+				apps_box = this.apps_box;
+			var _dropped = function(event) {
+				var height = self.desktop.height() - 70,
+					before = $(this).data("order"),
+					rows = Math.floor(height / 88),
+					row = Math.floor(event.pageY / 88),
+					col = Math.floor(event.pageX / 88),
+					after = col * rows + row;
+				if (before == after) {
+					$(this).css({
+						left: col * 88,
+						top: row * 88
+					});
+					return;
+				}
+				console.log(before, after);
+				self._adjust_apps_order(before, after);
+			}
+			for (var i = 0; i < apps.length; i++) {
+				var app = apps[i];
+				_apps[app.order] = $("<div>").app(app).appendTo(apps_box).draggable({
+					containment: this.desktop,
+					stop: _dropped
+				}).data("order", app.order);
+			}
+			this.apps = _apps
+		},
+
+		_adjust_apps_order: function(before, after) {
+			var apps = this.apps;
+
+			if (!(after in apps)) {
+				apps[after] = apps[before];
+				apps[after].data("order", after);
+				delete apps[before];
+			} else {
+				var temp;
+				if (before > after) {
+					temp = before;
+					before = after;
+					after = temp;
+				}
+				temp = apps[before];
+				delete apps[before];
+				for (var i = before + 1; i <= after; i++) {
+					if (i in apps) {
+						apps[i-1] = apps[i];
+						apps[i-1].data("order", i-1);
+					}
+					delete apps[i];
+				}
+				temp.data("order", after);
+				apps[after] = temp;
+			}
+			this._arrange_apps();
+		},
+
+		_arrange_apps: function() {
+			var height = this.desktop.height() - 70,
+				rows = Math.floor(height / 88),
+				apps_box = this.apps_box,
+				apps = this.apps;
+
+			for (var i in apps) {
+				var app = apps[i],
+					order = app.data("order"),
+					col = Math.floor(order / rows),
+					row = order % rows;
+				app.css({
+					left: col * 88,
+					top: row * 88
+				});
+			}
 		}
 	});
 })(jQuery);
