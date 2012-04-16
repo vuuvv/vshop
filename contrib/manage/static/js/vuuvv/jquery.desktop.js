852ba20fdec912c28f44ff4d2fc2079f65676bdd
Zeng Ying <vuuvv@qq.com>
2012-04-16 17:29:54 +0800

    arrage icon

--- contrib/manage/static/js/vuuvv/jquery.desktop.js
+++ contrib/manage/static/js/vuuvv/jquery.desktop.js
@@ -5,25 +5,35 @@
 		},
 
 		_create: function() {
+			var self = this;
 			this.apps = [];
-			for (var i = 0; i < 30; i++) {
+			for (var i = 0; i < 10; i++) {
 				this.apps.push({order: i * 2, img: 'http://2.web.qstatic.com/webqqpic/pubapps/0/2/images/big.png', title: '金山快盘' + i, action: $.noop});
 			}
 			this.desktop = this.element;
+			$(window).resize(function() {
+				self._arrange_apps();
+				self._arrange_dock();
+			});
 			this.apps_box = $("<div>").appendTo(this.desktop);
-			this._create_dock();
 			this._create_apps();
+			this._create_dock();
 			this._arrange_apps();
+			this._arrange_dock();
 		},
 
 		_create_dock: function() {
-			var dock = $("<div>").dock().appendTo(this.desktop).position({of: this.desktop, at: "bottom", my: "bottom"});
+			this.dock = $("<div>").dock().appendTo(this.desktop);
+		},
+
+		_arrange_dock: function() {
+			this.dock.position({of: this.desktop, at: "bottom", my: "bottom"});
 		},
 
 		_create_apps: function() {
 			var self = this,
 				apps = this.apps,
-				_apps = {},
+				_apps = [],
 				apps_box = this.apps_box;
 			var _dropped = function(event) {
 				var height = self.desktop.height() - 70,
@@ -39,45 +49,83 @@
 					});
 					return;
 				}
-				console.log(before, after);
 				self._adjust_apps_order(before, after);
 			}
 			for (var i = 0; i < apps.length; i++) {
 				var app = apps[i];
+				if (app.order >= _apps.length) {
+					this._extend_array(_apps, app.order);
+				}
 				_apps[app.order] = $("<div>").app(app).appendTo(apps_box).draggable({
 					containment: this.desktop,
 					stop: _dropped
+				}).click(function() {
+					$("<h1>TEST</h1>").window({theme: "vista"});
 				}).data("order", app.order);
 			}
 			this.apps = _apps
 		},
 
-		_adjust_apps_order: function(before, after) {
-			var apps = this.apps;
-
-			if (!(after in apps)) {
-				apps[after] = apps[before];
-				apps[after].data("order", after);
-				delete apps[before];
-			} else {
-				var temp;
-				if (before > after) {
-					temp = before;
-					before = after;
-					after = temp;
-				}
-				temp = apps[before];
-				delete apps[before];
-				for (var i = before + 1; i <= after; i++) {
-					if (i in apps) {
-						apps[i-1] = apps[i];
-						apps[i-1].data("order", i-1);
-					}
-					delete apps[i];
-				}
-				temp.data("order", after);
-				apps[after] = temp;
+		_extend_array: function(a, length) {
+			for (var i = a.length; i <= length; i++) {
+				a.push(null);
 			}
+		},
+
+		_insert_app: function(app, index) {
+			var apps = this.apps,
+				empty = index;
+			if (index >= apps.length) {
+				this._extend_array(apps, index);
+			}
+			while (apps[empty] !== null && empty < apps.length)
+				empty++;
+			if (empty == apps.length) {
+				apps.push(null);
+			}
+			while(empty !== index) {
+				this._replace_app(apps[empty - 1], empty);
+				empty = empty - 1;
+			}
+			this._replace_app(app, empty);
+		},
+
+		_remove_app: function(index) {
+			var app = this.apps[index];
+			this.apps[index] = null;
+			return app;
+		},
+
+		_replace_app: function(app, index) {
+			this.apps[index] = app;
+			if (app !== null)
+				app.data("order", index);
+		},
+
+		_append_app: function(app) {
+			var empty = 0,
+				apps = this.apps;
+			while (apps[empty] === null && empty < apps.length)
+				empty++;
+			if (empty == apps.length)
+				apps.push(app);
+			else
+				apps[empty] = app;
+			app.data("order", empty);
+		},
+
+		_adjust_apps_order: function(src, target) {
+			var apps = this.apps,
+				app = this._remove_app(src);
+			if (target >= apps.length) {
+				this._extend_array(apps, target);
+			}
+			if (apps[target] === null) {
+				this._replace_app(app, target);
+			} else {
+				this._insert_app(app, target);
+			}
+
 			this._arrange_apps();
 		},
 
@@ -87,15 +135,16 @@
 				apps_box = this.apps_box,
 				apps = this.apps;
 
-			for (var i in apps) {
-				var app = apps[i],
-					order = app.data("order"),
-					col = Math.floor(order / rows),
-					row = order % rows;
-				app.css({
-					left: col * 88,
-					top: row * 88
-				});
+			for (var i = 0; i < apps.length; i++) {
+				var app = apps[i];
+				if (app !== null) {
+					var col = Math.floor(i / rows),
+						row = i % rows;
+					app.css({
+						left: col * 88,
+						top: row * 88
+					});
+				}
 			}
 		}
 	});
